658bef16ec59aaa8dca340270b754cb680264d45
Sam Fay-Hunt <samfayhunt@gmail.com>
2021-03-08 23:27:32 +0000

    Basic stats/status effects implementation

---
+++ Dungeon-Game/Assets/Scripts/EntityStatus/BaseStatus.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseStatus : MonoBehaviour, IDamagable, IHealable, IKillable
+{
+    public int HP;
+    public int MaxHp;
+
+    public int MoveSpeed; // Movement speed modifier
+    private float moveSpeedPointValue = 0.1f;
+
+    public int AttackPower; // Outgoing damage modifier
+    private float attackPointValue = 0.1f;
+
+    public int Defense; // Incoming damage modifier
+    private float defPointValue = 0.1f;
+    
+    public List<Debuff> Debuffs;
+    public List<Buff> Buffs;
+
+    public void Damage(int damageTaken)
+    {
+        float modifiedDamage = damageTaken * (1 - (defPointValue * Defense));
+        if (modifiedDamage < 0)
+            modifiedDamage = 0f;
+        
+        HP -= (int) modifiedDamage;
+        if (HP <= 0)
+        {
+            Kill();
+        }
+    }
+
+    public void Heal(int healAmount)
+    {
+        HP += healAmount;
+        if (HP < MaxHp)
+            HP = MaxHp;
+    }
+
+    public void Kill()
+    {
+        Debug.Log("TODO: Kill this unit");
+    }
+
+    void Start()
+    {
+        StartCoroutine(checkStatusEffects());
+        StartCoroutine(applyContinousEffects());
+    }
+
+    private IEnumerator checkStatusEffects()
+    {
+        for (; ; )
+        {
+            foreach (Debuff debuff in Debuffs)
+            {
+                if (debuff.Expired())
+                {
+                    debuff.ClearStatus(this);
+                    Debuffs.Remove(debuff);
+                }
+            }
+            foreach (Buff buff in Buffs)
+            {
+                if (buff.Expired())
+                {
+                    buff.ClearStatus(this);
+                    Buffs.Remove(buff);
+                }
+            }
+
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    private IEnumerator applyContinousEffects()
+    {
+        for (; ; )
+        {
+            foreach (Buff buff in Buffs)
+            {
+                buff.ContinuousEffect(this);
+            }
+            foreach (Debuff debuff in Debuffs)
+            {
+                debuff.ContinuousEffect(this);               
+            }
+
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+}
---
+++ Dungeon-Game/Assets/Scripts/EntityStatus/StatusEffect.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StatusEffect
+{
+    protected bool stackable;
+    private float startTime;
+    private float duration;
+
+    public StatusEffect(float duration, bool stackable)
+    {
+        this.stackable = stackable;
+        this.duration = duration;
+        startTime = Time.time;
+    }
+
+    public bool Expired()
+    {
+        if (Time.time - startTime > duration)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    protected virtual void overwrite(List<StatusEffect> statuses) { }
+
+    public virtual void ContinuousEffect(BaseStatus status) { }
+
+    public abstract void ApplyStatus(BaseStatus status);
+
+    public abstract void ClearStatus(BaseStatus status);
+}
+
+public abstract class Debuff : StatusEffect
+{
+    public Debuff(float duration, bool stackable) : base(duration, stackable)
+    {
+
+    }
+}
+
+public abstract class Buff : StatusEffect
+{
+    public Buff(float duration, bool stackable) : base(duration, stackable)
+    {
+
+    }
+}
+
+public class SlowStatus: Debuff
+{
+    private int slowAmount;
+
+    public SlowStatus(int slowAmount, float duration) : base(duration, true)
+    {
+        this.slowAmount = slowAmount;
+    }
+
+    public override void ApplyStatus(BaseStatus status)
+    {
+        status.MoveSpeed -= slowAmount;
+    }
+
+    public override void ClearStatus(BaseStatus status)
+    {
+        status.MoveSpeed += slowAmount;
+    }
+}
+
+
+public class PoisonedStatus : Debuff
+{
+    private int damageTick;
+
+    public PoisonedStatus(int damageTick, float duration) : base(duration, false)
+    {
+        this.damageTick = damageTick;
+    }
+
+    public override void ContinuousEffect(BaseStatus status)
+    {
+        base.ContinuousEffect(status);
+        status.Damage(damageTick);
+    }
+
+    public override void ApplyStatus(BaseStatus status)
+    {
+
+    }
+
+    public override void ClearStatus(BaseStatus status)
+    {
+
+    }
+}
+
+public class WeakenStatus : Debuff
+{
+    private int attackReduction;
+    public WeakenStatus(int attackReduction, float duration) : base(duration, false)
+    {
+        this.attackReduction = attackReduction;
+    }
+
+    protected override void overwrite(List<StatusEffect> statuses)
+    {
+        base.overwrite(statuses);
+        foreach (Debuff debuff in statuses)
+        {
+            // TODO implement overwrite
+        }
+    }
+
+    public override void ApplyStatus(BaseStatus status)
+    {
+        
+    }
+
+    public override void ClearStatus(BaseStatus status)
+    {
+        throw new System.NotImplementedException();
+    }
+}
