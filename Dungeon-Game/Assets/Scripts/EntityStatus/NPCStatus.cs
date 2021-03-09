a56436d1aa614e0ba1d5418756181b4c2cc63a8c
Sam Fay-Hunt <samfayhunt@gmail.com>
2021-03-09 16:00:20 +0000

    NPC & Player specific status' setup

--- Dungeon-Game/Assets/Scripts/EntityStatus/NPCStatus.cs
+++ Dungeon-Game/Assets/Scripts/EntityStatus/NPCStatus.cs
@@ -2,17 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPCStatus : MonoBehaviour
+public class NPCStatus : BaseStatus
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // NPC unique status stuff here
 }
--- Dungeon-Game/Assets/Scripts/EntityStatus/PlayerStatus.cs
+++ Dungeon-Game/Assets/Scripts/EntityStatus/PlayerStatus.cs
@@ -2,17 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerStatus : MonoBehaviour
+public class PlayerStatus : BaseStatus
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // Status stuff unique to player here
 }
