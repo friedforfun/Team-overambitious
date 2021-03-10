1113799931b2d3dd35afaf3acab32bc51d57043c
Sam Fay-Hunt <samfayhunt@gmail.com>
2021-03-10 23:11:15 +0000

    Added base for combat behaviours

---
+++ Dungeon-Game/Assets/Scripts/Combat/BasicAttack.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Handles frequency of attacks and damage
+/// </summary>
+public class BasicAttack : MonoBehaviour
+{
+
+    [SerializeField] private BaseStatus stats; // for damage/fire-rate ect
+
+    // child classes should override this method to define attack behaviour (melee, shoot projectile ect)
+    public virtual void PerformAttack(Vector3 direction, int attackPower)
+    {
+        Debug.LogWarning("Attack not implemented");
+    } 
+
+    private bool canAttack = true;
+
+
+    public void Attack(Vector3 direction)
+    {
+        if (canAttack)
+        {
+            canAttack = false;
+
+            PerformAttack(direction, stats.AttackPower);
+
+            StartCoroutine(enableAttack());
+        }
+    }
+
+    private IEnumerator enableAttack()
+    {
+        yield return new WaitForSeconds(stats.AttackInterval());
+        canAttack = true;
+    }
+}
