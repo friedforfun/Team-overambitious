efe96a6516ba617226fe160128213d8af227a9ff
Keir Edwards <keire16@hotmail.co.uk>
2021-03-11 12:53:41 +0000

    Added Level Generation Unity Project

---
+++ Dungeon-Game/Assets/Scripts/RoomSpawn.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawn : MonoBehaviour
+{
+    public int openingDirection;
+    //1 = top
+    //2 = bottom
+    //3 = right
+    //4 = left
+
+    private RoomTemplates Templates;
+    private int rand;
+    private bool spawned = false;
+
+    void Start()
+    {
+        Templates = GameObject.Find("ScriptHolder").GetComponent<RoomTemplates>();
+        Invoke("Spawn", 0.1f);
+    }
+
+    void Spawn()
+    {
+        if (spawned == false) {
+            if (openingDirection == 1)
+            {
+                rand = Random.Range(0, Templates.BottomRooms.Length);
+                Instantiate(Templates.BottomRooms[rand], transform.position, Quaternion.identity);      //Quaternion.Identity could be changed to allow for different rotations of rooms.
+        }
+            else if (openingDirection == 2)
+            {
+                rand = Random.Range(0, Templates.FrontRooms.Length);
+                Instantiate(Templates.FrontRooms[rand], transform.position, Quaternion.identity);
+            }
+            else if (openingDirection == 3)
+            {
+                rand = Random.Range(0, Templates.LeftRooms.Length);
+                Instantiate(Templates.LeftRooms[rand], transform.position, Quaternion.identity);
+            }
+            else if (openingDirection == 4)
+            {
+                rand = Random.Range(0, Templates.RightRooms.Length);
+                Instantiate(Templates.RightRooms[rand], transform.position, Quaternion.identity);
+            }
+
+            Templates.RoomCount += 1;
+            spawned = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("SpawnTag") && other.GetComponent<RoomSpawn>().spawned == true)
+        {
+            Destroy(gameObject);
+
+        }
+    }
+}
---
+++ Dungeon-Game/Assets/Scripts/RoomTemplates.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomTemplates : MonoBehaviour
+{
+    public GameObject[] FrontRooms;
+    public GameObject[] BottomRooms;
+    public GameObject[] LeftRooms;
+    public GameObject[] RightRooms;
+
+    public int RoomCount = 0;
+    public int MinimumRooms;
+    public int MaximumRooms;
+    private int RoomCountTemp = 0;
+    public int MaxFramesNoRooms;
+    private int FramesNoRooms = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(RoomCount);
+       if(RoomCountTemp == RoomCount)
+        {
+            FramesNoRooms += 1;
+            if(FramesNoRooms >= MaxFramesNoRooms && (RoomCount < MinimumRooms || RoomCount > MaximumRooms))
+            {
+           
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+        RoomCountTemp = RoomCount;
+        
+        
+    }
+}
