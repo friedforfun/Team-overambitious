34dd9f0ef99651ce3619d1f54d289358e8f64a49
Sam Fay-Hunt <samfayhunt@gmail.com>
2021-03-12 12:09:28 +0000

    Imported robot sphere npc

---
+++ Dungeon-Game/Assets/Scripts/NPC/State/GeneralNPCStates.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public abstract class NPCOutOfCombat : NPCBaseState
+{
+    protected ExampleStateMachine exampleState;
+    private float startTime;
+    private float duration;
+    public NPCOutOfCombat(GameObject npc) : base(npc)
+    {
+        startTime = Time.time;
+        duration = Random.Range(2f, 8f);
+        exampleState = npc.GetComponent<ExampleStateMachine>();
+    }
+
+    protected bool stateExpired()
+    {
+        if (Time.time - startTime > duration)
+        {
+            return true;
+        }
+        return false;
+    }
+}
+
+/// <summary>
+/// Wandering state (NPC wanders aimlessly)
+/// </summary>
+public class NPCWander : NPCOutOfCombat
+{
+    public NPCWander(GameObject npc) : base(npc)
+    {
+
+    }
+
+    public override void UpdateState()
+    {
+        if (stateExpired())
+            exampleState.SetState(new NPCIdle(npc));
+    }
+}
+
+/// <summary>
+/// Idle behaviour (NPC stands still)
+/// </summary>
+public class NPCIdle : NPCOutOfCombat
+{
+    public NPCIdle(GameObject npc) : base(npc)
+    {
+
+    }
+
+    public override void UpdateState()
+    {
+        if (stateExpired())
+            exampleState.SetState(new NPCWander(npc));
+    }
+}
+
---
+++ Dungeon-Game/Assets/Scripts/PlayerScripts/PlayerStatus.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : BaseStatus
+{
+    // Status stuff unique to player here
+}
