4c3302aad549cedacf10250ffe7d040f0d0046e1
Keir Edwards <keire16@hotmail.co.uk>
2021-03-12 22:13:32 +0000

    Removed Collisions from Doors - Added Collisions to Walls

--- Dungeon-Game/Assets/Scripts/RoomSpawn.cs
+++ Dungeon-Game/Assets/Scripts/RoomSpawn.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RoomSpawn : MonoBehaviour
 {
@@ -45,6 +46,10 @@ public class RoomSpawn : MonoBehaviour
             }
 
             Templates.RoomCount += 1;
+            if(Templates.RoomCount > Templates.MaximumRooms)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
             spawned = true;
         }
     }
