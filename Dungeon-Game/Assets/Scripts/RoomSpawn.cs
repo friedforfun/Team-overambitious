67ea1bb87f692316a89d35563f540ee1261c1771
Keir Edwards <keire16@hotmail.co.uk>
2021-03-13 12:16:27 +0000

    Added Mirroring of Level
    Still tweaking to do to avoid levels clipping if too many rooms.

--- Dungeon-Game/Assets/Scripts/RoomSpawn.cs
+++ Dungeon-Game/Assets/Scripts/RoomSpawn.cs
@@ -14,11 +14,17 @@ public class RoomSpawn : MonoBehaviour
     private RoomTemplates Templates;
     private int rand;
     private bool spawned = false;
+    private GameObject Scene;
 
     void Start()
     {
         Templates = GameObject.Find("ScriptHolder").GetComponent<RoomTemplates>();
-        Invoke("Spawn", 0.1f);
+
+        if (Templates.GenerationComplete == false)
+        {
+            Invoke("Spawn", 0.1f);
+        }
+        Scene = Templates.LeftScene;
     }
 
     void Spawn()
@@ -27,22 +33,22 @@ public class RoomSpawn : MonoBehaviour
             if (openingDirection == 1)
             {
                 rand = Random.Range(0, Templates.BottomRooms.Length);
-                Instantiate(Templates.BottomRooms[rand], transform.position, Quaternion.identity);      //Quaternion.Identity could be changed to allow for different rotations of rooms.
+                Instantiate(Templates.BottomRooms[rand], transform.position, Quaternion.identity, Scene.transform);      //Quaternion.Identity could be changed to allow for different rotations of rooms.
         }
             else if (openingDirection == 2)
             {
                 rand = Random.Range(0, Templates.FrontRooms.Length);
-                Instantiate(Templates.FrontRooms[rand], transform.position, Quaternion.identity);
+                Instantiate(Templates.FrontRooms[rand], transform.position, Quaternion.identity, Scene.transform);
             }
             else if (openingDirection == 3)
             {
                 rand = Random.Range(0, Templates.LeftRooms.Length);
-                Instantiate(Templates.LeftRooms[rand], transform.position, Quaternion.identity);
+                Instantiate(Templates.LeftRooms[rand], transform.position, Quaternion.identity, Scene.transform);
             }
             else if (openingDirection == 4)
             {
                 rand = Random.Range(0, Templates.RightRooms.Length);
-                Instantiate(Templates.RightRooms[rand], transform.position, Quaternion.identity);
+                Instantiate(Templates.RightRooms[rand], transform.position, Quaternion.identity, Scene.transform);
             }
 
             Templates.RoomCount += 1;
@@ -51,6 +57,7 @@ public class RoomSpawn : MonoBehaviour
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             }
             spawned = true;
+
         }
     }
 
