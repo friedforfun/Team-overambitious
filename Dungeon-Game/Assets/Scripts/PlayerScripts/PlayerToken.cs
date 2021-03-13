4824cb709adbfc90515f6ce6102f0efa036e0d6a
Callum Jones <callum72gj19@gmail.com>
2021-03-13 14:50:24 +0000

    Changed mini-map tokens

---
+++ Dungeon-Game/Assets/Scripts/PlayerScripts/PlayerToken.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerToken : MonoBehaviour
+{
+
+    public GameObject player;
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position;
+    }
+}
