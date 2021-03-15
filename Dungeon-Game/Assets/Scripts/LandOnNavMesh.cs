f903070e9939942e3074b3723d034f605d143852
Sam Fay-Hunt <samfayhunt@gmail.com>
2021-03-15 15:33:29 +0000

    Added script to help place npc on navmesh

---
+++ Dungeon-Game/Assets/Scripts/LandOnNavMesh.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+/// <summary>
+/// Put this script on gameobjects that are having trouble landing on the navmesh
+/// https://forum.unity.com/threads/failed-to-create-agent-because-it-is-not-close-enough-to-the-navmesh.125593/
+/// </summary>
+public class LandOnNavMesh : MonoBehaviour
+{
+    [SerializeField] float MaxDistance = 500f;
+
+    void Start()
+    {
+        NavMeshHit closestPoint;
+        if (NavMesh.SamplePosition(transform.position , out closestPoint, MaxDistance, NavMesh.AllAreas))
+        {
+            transform.position = closestPoint.position;
+        }
+        else
+        {
+            Debug.LogError("Count not find a point on the NavMesh");
+        }
+    }
+
+}
