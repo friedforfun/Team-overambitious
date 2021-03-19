14501100cd3a6a881ccc8643b16a0af01770b5f8
jimmym620 <jimmym620@gmail.com>
2021-03-19 19:46:27 +0000

    add spike prefab - needs damage

---
+++ Dungeon-Game/Assets/Scripts/NPC/Traps/TrapAttack.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapAttack : BasicAttack
+{
+    private GameObject spike;
+    public bool active;
+
+    public override void PerformAttack(Vector3 direction, float attackPower)
+    {
+        
+    }
+
+
+}
