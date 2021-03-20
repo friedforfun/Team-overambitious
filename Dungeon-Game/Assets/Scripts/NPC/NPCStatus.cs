51c685f8f2792784ebd16348fce1c0534ecc6411
Sam Fay-Hunt <samfayhunt@gmail.com>
2021-03-20 14:32:32 +0000

    Added event callback to damage taken

--- Dungeon-Game/Assets/Scripts/NPC/NPCStatus.cs
+++ Dungeon-Game/Assets/Scripts/NPC/NPCStatus.cs
@@ -5,4 +5,15 @@ using UnityEngine;
 public class NPCStatus : BaseStatus
 {
     // NPC unique status stuff here
+    private void Start()
+    {
+        OnDamageTaken += () => npcDamage();
+    }
+
+    private void npcDamage()
+    {
+        if (myMiniBar != null) Destroy(myMiniBar);
+        myMiniBar = Instantiate(miniBar, new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z + 1f), Quaternion.identity);
+        myMiniBar.GetComponent<MiniBar>().multiplier = (float)HP / MaxHp;
+    }
 }
