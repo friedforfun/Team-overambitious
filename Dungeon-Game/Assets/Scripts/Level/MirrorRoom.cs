a3911466b898730504d0435fd39f217f66a5f4e5
Keir Edwards <keire16@hotmail.co.uk>
2021-03-23 18:32:11 +0000

    Moved Right Level
    Right level now spawns +500 to the right of the left level which it mirrors.

--- Dungeon-Game/Assets/Scripts/Level/MirrorRoom.cs
+++ Dungeon-Game/Assets/Scripts/Level/MirrorRoom.cs
@@ -20,7 +20,7 @@ public class MirrorRoom : MonoBehaviour
         if(Templates.GenerationComplete == true && SceneFlipped == false && Templates.BossSpawned == true )
         {
             Templates.MaximumRooms = Templates.MaximumRooms * 2;
-            GameObject SceneRight = Instantiate(SceneLeft, new Vector3(100, 0, 0), Quaternion.identity);
+            GameObject SceneRight = Instantiate(SceneLeft, new Vector3(500, 0, 0), Quaternion.identity);
             SceneRight.name = "SceneRight";
             SceneRight.transform.parent = GameObject.Find("Scenes").transform;
             SceneFlipped = true;
