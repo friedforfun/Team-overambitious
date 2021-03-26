e4c1835162d10c020adefc0df159868b135800ac
Sam Fay-Hunt <samfayhunt@gmail.com>
2021-03-26 11:01:14 +0000

    Added stone wall texture to walls

--- Dungeon-Game/Assets/Scripts/PlayerScripts/PlayerStatus.cs
+++ Dungeon-Game/Assets/Scripts/PlayerScripts/PlayerStatus.cs
@@ -14,20 +14,30 @@ public class PlayerStatus : BaseStatus
 
     [SerializeField] private Team team;
     [SerializeField] private string playerEndgame;
+    [SerializeField] private Transform goTransform;
 
     private Transform FinaleSpawnPoint;
-   
+
+    public bool IsInBossRoom = false;
 
     public void TriggerEndGame()
     {
         FinaleSpawnPoint = FindObjectOfType<FinaleRoom>().GetPlayerSpawn(team);
-        EventManager.TriggerEvent(playerEndgame);
-        WarpToFinale();
+        
+        StartCoroutine(Warp());
     }
 
-    private void WarpToFinale()
+    public IEnumerator Warp()
     {
-        gameObject.transform.position = FinaleSpawnPoint.position;
+        yield return new WaitForSeconds(0.2f);
+        goTransform.position = new Vector3(FinaleSpawnPoint.position.x, FinaleSpawnPoint.position.y + 0.5f, FinaleSpawnPoint.position.z);
+        if (goTransform.position.x > FinaleSpawnPoint.position.x - 5f || goTransform.position.x < FinaleSpawnPoint.position.x + 5f)
+        {
+            if (!IsInBossRoom)
+                EventManager.TriggerEvent(playerEndgame);
+            IsInBossRoom = true;
+        }
+            
     }
 
 }
