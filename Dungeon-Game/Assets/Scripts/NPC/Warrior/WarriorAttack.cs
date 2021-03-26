30fb7b103aa362fafe26225d4b5542e286267e0c
jimmym620 <jimmym620@gmail.com>
2021-03-26 10:44:18 +0000

    Add basic melee npc

---
+++ Dungeon-Game/Assets/Scripts/NPC/Warrior/WarriorAttack.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarriorAttack : BasicAttack
+{
+    [SerializeField] private GameObject Hitpoint;
+
+    public override void PerformAttack(Vector3 direction, float attackPower)
+    {
+       
+        
+
+    }
+
+}
